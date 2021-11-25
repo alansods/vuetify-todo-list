aa2bc6f076996118f7e46cb21225760138a896fa
Alan Santos <alansods@gmail.com>
2021-11-25 13:26:28 -0300

    snackbar added

--- src/components/tarefas/ConfigTask.vue
+++ src/components/tarefas/ConfigTask.vue
@@ -8,10 +8,20 @@
 
     <v-list>
       <v-list-item>
-        <v-list-item-title>Editar</v-list-item-title>
+        <v-list-item-icon class="mr-2">
+          <v-icon>mdi-pencil-box-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Editar</v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
-      <v-list-item @click="$store.commit('deleteTask', task.id)">
-        <v-list-item-title>Excluir</v-list-item-title>
+      <v-list-item @click="$store.dispatch('deleteTask', task.id)">
+        <v-list-item-icon class="mr-2">
+          <v-icon>mdi-delete</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Excluir</v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
     </v-list>
   </v-menu>
